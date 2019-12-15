cc6739290d93a65bb89bf24a5130a4553109a33f
simranjeetkaur <chakkalsimmi@gmail.com>
2019-12-15 10:09:30 -0500

    changing label change in wing activity

--- app/src/main/java/com/example/a4s_finalproject/homepagescrolling.java
+++ app/src/main/java/com/example/a4s_finalproject/homepagescrolling.java
@@ -73,7 +73,11 @@ public class homepagescrolling extends AppCompatActivity {
         lyt_wings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), WingsList.class);
+                Intent intent = new Intent(getApplicationContext(), Bookingactivity.class);
+                String text = "Wings";
+                String price = "35";
+                intent.putExtra("price",price);
+                intent.putExtra("PIZZANAME",text);
                 startActivity(intent);
             }
         });
@@ -96,6 +100,7 @@ public class homepagescrolling extends AppCompatActivity {
         lyt_soda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(getApplicationContext(), sodapopactivity.class);
                 startActivity(intent);
             }
